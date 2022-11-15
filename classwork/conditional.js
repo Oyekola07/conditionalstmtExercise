c80e37e93692215b2928ae16b6efb22d2b3e77ad
ADELAKUN <adelakunoyekola09@gmail.com>
2022-11-15 13:52:27 +0100

    classExercise

---
+++ classwork/conditional.js
@@ -0,0 +1,23 @@
+let score = 39
+if(score >= 70 && score <= 100){
+    console.log('Grade A')
+}else if(score >= 60 && score <=69){
+    console.log('Grade B')
+}else if(score >= 50 && score <= 59){
+    console.log('Grade C')
+}else if(score >=45 && score <= 49){
+    console.log('Grade D')
+}else if(score >= 40 && score <= 45){
+    console.log('Grade E')
+    
+}else if(score >= 0 && score <= 40){
+    console.log('You failed')
+}
+
+else{
+    console.log('This is an invalid score')
+}
+
+
+
+   
